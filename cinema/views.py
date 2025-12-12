0c27733cdd691018bd1e7289ecfae9fc0d229aaa
Vitalii Stetsenko <stvitalik01062006@gmail.com>
2025-12-12 22:27:04 +0100

    solution3

--- cinema/views.py
+++ cinema/views.py
@@ -1,10 +1,20 @@
 from rest_framework.response import Response
-from rest_framework import status, viewsets, mixins, generics
+from rest_framework import (
+    status,
+    viewsets,
+    mixins,
+    generics
+)
 
 from django.shortcuts import get_object_or_404
 from rest_framework.views import APIView
 
-from cinema.models import Movie, Genre, Actor, CinemaHall
+from cinema.models import (
+    Movie,
+    Genre,
+    Actor,
+    CinemaHall
+)
 from cinema.serializers import (
     MovieSerializer,
     GenreSerializer,
